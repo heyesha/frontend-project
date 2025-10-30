c65b4a27b35a95a52fe8684940f1dcc228629bd0
Rodion <rodion.kondrashin@bk.ru>
2025-10-30 21:52:10 +0700

    feat: бургер меню

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 //import './index.css';
+
+const menu = document.querySelector('.mobile-menu__inner');
+
+document.querySelector('.js-menu-active').addEventListener('click', () => {
+  menu.classList.toggle('mobile-menu__inner--active');
+});
